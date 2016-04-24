c6c1b27deafe4a23e4cb36dd68a4434880b4e9ad
AngieKarina <angiekarina35@gmail.com>
2016-04-23 23:21:04 -0600

    Cambio de Carpeta

--- intercambealo/app/models/product.rb
+++ intercambealo/app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
     validates :name, presence: {:message => "You must introduce a name"}
-    validates :descripcion, presence: {:message => "You must introduce a description"}  
+    validates :description, presence: {:message => "You must introduce a description"}  
     validates :state, presence: {:message => "You must introduce a state"}  
 end
