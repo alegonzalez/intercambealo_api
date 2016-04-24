94c50b9cfd88d6f0589b16b600e99c03dde89828
AngieKarina <angiekarina35@gmail.com>
2016-04-23 22:50:19 -0600

    Obtiene Productos

--- client_intercambealo/getProduct.php
+++ client_intercambealo/getProduct.php
@@ -1,6 +1,8 @@
 <?php 
 $response = file_get_contents('http://localhost:3000/product.json');
 $response = json_decode($response);
+
+
 if($response === FALSE){
 	header('Content-Type: application/json');
 	echo json_encode(array('message' => "error"));
