b3ca13e9bf44bbfc1be19f4ac055bbb4ede9e11d
AngieKarina <angiekarina35@gmail.com>
2016-04-24 21:30:05 -0600

    Obtener los productos por usuario

--- client_intercambealo/getProductUser.php
+++ client_intercambealo/getProductUser.php
@@ -1,8 +1,15 @@
 <?php 
 
-$response = @file_get_contents('http://localhost:3000/product.json');
-$response = json_decode($response);
+//Hay que mandar el user_id para traer los productos de esa usuario
+$user_id = $_GET['id'];
 
+
+$json_url = "http://localhost:3000/product/productUser";
+$json= file_get_contents($json_url."?user_id=".$user_id);
+
+
+echo $json;
+die;
 
 if($response === FALSE){
 	header('Content-Type: application/json');
@@ -13,3 +20,4 @@ if($response === FALSE){
 	header('Content-Type: application/json');
 	echo json_encode(array('message' => $response));
 }
+
